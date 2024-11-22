f65fa002b2decb91f9cf5d196b47c7ef4e1a144e
Harshit Parmar <parmarharshit2402@gmail.com>
2024-11-22 11:29:56 +0530

    learn about nums and Math

--- nums_and_math.js
+++ nums_and_math.js
@@ -1,2 +1,27 @@
 const score = 400;
+console.log(score);
+
+const balance = new Number(100) //specify the type here 
 console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(1));  //For Precision Value
+
+const otherNumber = 23.85461856;
+console.log(otherNumber.toPrecision(2)); //For Precision Value
+
+//++++++++++++++++++++++++++++++++++++++++++  MATHS ++++++++++++++++++++++++++++++++++++++
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.5));
+
+console.log(Math.random());
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min); // random values in range
+
+
+
